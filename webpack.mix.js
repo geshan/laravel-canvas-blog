3d41e9170ad54e448a226b6895834dcf3a18df3d
Geshan Manandhar <geshan@simplywallst.com>
2023-06-17 09:28:27 +1000

    Laravel blog with canvas

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+mix.js('resources/js/canvas-ui/app.js', 'public/js/canvas-ui.js').vue()
+    .sass('resources/sass/canvas-ui.scss', 'public/css/canvas-ui.css');
+mix.js('resources/js/canvas-ui/app.js', 'public/js/canvas-ui.js').vue()
+    .sass('resources/sass/canvas-ui.scss', 'public/css/canvas-ui.css');
+mix.js('resources/js/canvas-ui/app.js', 'public/js/canvas-ui.js').vue()
+    .sass('resources/sass/canvas-ui.scss', 'public/css/canvas-ui.css');
